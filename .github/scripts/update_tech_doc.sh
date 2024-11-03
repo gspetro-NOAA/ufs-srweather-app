8f11d3d5e542c28a98303f66d244770c2edc1e00
gspetro-NOAA <gillian.petro@noaa.gov>
2024-11-02 23:48:10 -0400

    update script name; add build step

--- .github/scripts/update_tech_doc.sh
+++ .github/scripts/update_tech_doc.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 # This script rebuilds technical documentation for the ush and tests/WE2E Python scripts
 # The build will fail if there are warnings. 
-# Warnings may be cause by incomplete or nonexistent documentation
+# Warnings may be caused by incomplete or nonexistent documentation
 # as well as by minor issues such as broken external links that need to be fixed or removed
-
 
 # Install prerequisites
 sudo apt-get install python3-sphinx
-#sphinx_rtd_theme
-#sphinxcontrib-bibtex
-
 
 # Remove existing directories
 cd doc/TechDocs
 rm -rf ush
-rm -rf tests
+rm -rf tests/WE2E
 echo $PWD
 
-# Regenerate docs in ush and tests/WE2E based on current state of scripts in those directories.
+# Regenerate tech docs in ush and tests/WE2E based on current state of scripts in those directories.
 sphinx-apidoc -fM -o ./ush ../../ush
 sphinx-apidoc -fM -o ./tests/WE2E ../../tests/WE2E
 ls
-# Need to check if there's a mismatch between what comes out of this build 
-# and what is in the PR. 
 
+# Check for mismatch between what comes out of this action and what is in the PR. 
 git diff
+
 
 # May be able eventually to add an action that adds the properly built docs to the PR or the target branch
 
